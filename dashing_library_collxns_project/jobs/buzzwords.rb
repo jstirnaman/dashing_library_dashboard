4f752fb9cb4ca5f87afa2019a0e9a29061e13c9b
Jason Stirnaman <stirnamanj@gmail.com>
2015-04-01 08:51:28 -0500

    Parse incoming JSON from Voyager SQL for Buzzwords term counts.

--- dashing_library_collxns_project/jobs/buzzwords.rb
+++ dashing_library_collxns_project/jobs/buzzwords.rb
@@ -4,10 +4,9 @@
 SCHEDULER.every '5s' do
   location_counts = Hash.new
   # Convert some JSON back to a Ruby array of hashes
-  IO.foreach("data/sql_items_in_building.json") do |line|
-    (JSON.parse line).first(10).each {|h| location_counts[h["x"]] = { label: h["x"].sub("Dykes", '').strip, value: h["y"] } }
+  IO.foreach("data/sql_items_in_bldg.json", "\n") do |line|
+    (JSON.parse line).to_chart("LOCATION", "ITEMS").first(10).each {|h| location_counts[h[:x]] = { label: h[:x].sub("Dykes", '').strip, value: h[:y] } }
   end
-#   location_counts[random_buzzword] = { label: random_buzzword, value: (buzzword_counts[random_buzzword][:value] + 1) % 30 }
   
   send_event('buzzwords', { items: location_counts.values })
 end
