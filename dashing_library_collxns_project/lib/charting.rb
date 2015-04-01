1e5e168ff312c9539f7ea5f721deb4f723d227a1
Jason Stirnaman <stirnamanj@gmail.com>
2015-04-01 08:54:03 -0500

    Helper methods for formatting incoming x, y coordinate data as a chart.

--- dashing_library_collxns_project/lib/charting.rb
+++ dashing_library_collxns_project/lib/charting.rb
@@ -29,39 +29,44 @@ def to_time_chart(x = nil, y = nil)
   # req.body = {:auth_token => 'YOUR_AUTH_TOKEN', :points => [{:x=>1383022800, :y=>51}, {:x=>1383109200, :y=>34}, {:x=>1383195600, :y=>20}]}.to_json
 end
 
-		def to_chart
-			data = self.query_result.map do |h| 
-							 unless h.nil?
-								 points = {}
+  def to_chart(x = nil, y = nil)
+    data = self.map do |h| 
+             unless h.nil?
+               points = {}
+               if !x.nil? && !y.nil?
+                 points[:x] = h[x]
+                 points[:y] = h[y] ? try_convert_to_rickshaw(h[y]).to_i : "0" 
+               else
 								 a = h.to_a
 								 x = a[0][1]
 								 points[:x] = x ? x : ""
 								 y = a[1][1]
 								 points[:y] = y ? try_convert_to_rickshaw(y).to_i : "0"
-								 points
-							 end
-			end
-		end
-		
-		def try_convert_to_rickshaw(value)
-			# Dashing uses Rickshaw.js for graphing time series.
-			# Rickshaw expects time values in epoch seconds.
-			if value.class == BigDecimal
-				value = value
-			elsif value.class == Time
-				value = value.strftime('%s')
-			elsif value.class == String
-				dateparts = value.match(/(\d{4}).?(\d{2}).?(\d{2})/)
-				if !dateparts.nil?
-					# We have datetime values converted to a string.
-					date = Time.new(dateparts[1], dateparts[2], dateparts[3])
-					value = date.strftime('%s')
-				elsif !value.match(/^\d.*E\d.*/).nil?
-				  # We have BigDecimal values converted to a string.
-				  value = BigDecimal.new(value)
-				end
-			end
-			value 
-		end	
+               end
+               points
+             end
+    end
+  end
+    
+    def try_convert_to_rickshaw(value)
+      # Dashing uses Rickshaw.js for graphing time series.
+      # Rickshaw expects time values in epoch seconds.
+      if value.class == BigDecimal
+        value = value
+      elsif value.class == Time
+        value = value.strftime('%s')
+      elsif value.class == String
+        dateparts = value.match(/(\d{4}).?(\d{2}).?(\d{2})/)
+        if !dateparts.nil?
+          # We have datetime values converted to a string.
+          date = Time.new(dateparts[1], dateparts[2], dateparts[3])
+          value = date.strftime('%s')
+        elsif !value.match(/^\d.*E\d.*/).nil?
+          # We have BigDecimal values converted to a string.
+          value = BigDecimal.new(value)
+        end
+      end
+      value 
+    end  
 
 end
